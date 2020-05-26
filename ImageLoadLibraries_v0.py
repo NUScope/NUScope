12a46193d064c27aec0665f7284944bf36fc1257
nwk5405 <64105982+nwk5405@users.noreply.github.com>
2020-05-26 13:03:22 -0500

    Add files via upload
    First attempt at GUI with basic image load libraries. Note:
    - Still need to add save to HDF5
    - Still need to fix first metadata cell issue
    - etc

---
+++ ImageLoadLibraries_v0.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: Nathaniel Kabat
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage
+from skimage import io
+import pySPM
+
+
+def GenericImageOpenTIFF(filename):
+    '''
+    Generic TIFF Open function
+    '''      
+    meta_data = {"e1":"TEST1","e2":"TEST"}
+    image_data = io.imread(filename)
+    image_data = skimage.img_as_float(image_data)
+    return image_data,meta_data
+
+def HitachiSEMImageLoad(filename,meta_filename):
+    '''
+    This SHOULD work for the the follwing Hitach SEM models:   
+        - 4800 (VERIFIED as .TIFF + Metadata .txt)
+        - 3400 (NOT verified)
+        - 8030 (Partially verified)
+    
+    You must use the original TIFF file as exported from the SEM. 
+    This code assumes that the metadata file has the same name as the image file (This is how the microscope exports)
+     
+        
+    '''    
+    meta_data = {}
+    with open(meta_filename) as f:
+        keys = []
+        values = []
+        next(f)
+        for line in f:
+            templine = line.strip("\n").split("=")
+            keys.append(templine[0])
+            values.append(templine[1])
+    meta_data = dict(zip(keys,values))   
+    
+    image_data = io.imread(filename)
+    image_data = skimage.img_as_float(image_data)
+    return image_data,meta_data
+    
+
+def QuantaSEMImageLoad(filename,meta_filename):
+    '''
+    This SHOULD work with the following FEI SEM Models:
+        - Quanta 650
+        - TBD
+    This function requires:
+        - the original TIFF (or other image format) as exported from the SEM
+        - a text file with the COPIED metadata that is embedded at the end of the original tiff file. 
+    
+    '''
+    meta_data = {}
+    with open(meta_filename) as f:
+        keys = []
+        values = []
+        next(f)
+        for line in f:
+            templine = line.strip("\n").split("=")
+            print(templine)
+            try:
+                keys.append(templine[0])
+            except:
+                keys.append("")
+            try:
+                values.append(templine[1])
+            except:
+                values.append("")
+    meta_data = dict(zip(keys,values))
+
+    image_data = io.imread(filename)
+    image_data = skimage.img_as_float(image_data)
+    return image_data,meta_data
+
+
+
+def JEOL7900SEMImageLoad(filename,meta_filename):
+    '''
+    This SHOULD work for the following JEOL SEM Models:
+        - 7900
+        - TBD
+    This function requires:
+        - The original TIFF (or other image format) as exported from the SEM
+        - The metadata file (as exported by the SEM)   
+    '''
+    meta_data = {}
+    with open(meta_filename) as f:
+        keys = []
+        values = []
+        for line in f:
+            templine = line.strip("\n").strip("$").split(" ")
+            if len(templine) <= 2:
+                tempValues = templine[-1]
+            else:
+                tempValues = [i for i in templine[1:]]
+            try:
+                keys.append(templine[0])
+            except:
+                keys.append("")
+            try:
+                values.append(tempValues)
+            except:
+                values.append("")
+    meta_data = dict(zip(keys,values))
+    
+    image_data = io.imread(filename)
+    image_data = skimage.img_as_float(image_data)
+    return image_data,meta_data
+
+
+def BrukerAFMImageLoad(filename):
+    import pySPM
+    scan = pySPM.Bruker(filename)
+    meta_data = scan.layers[0]
+    new_meta_data ={}
+    for item in meta_data.items():
+        tempValue = item[1][0].decode('UTF-8')
+        print(tempValue)
+        new_meta_data[item[0].decode('UTF-8')]=tempValue   
+    try:
+        scan_data = scan.get_channel()
+    except:
+        scan_data = scan.get_channel(backward=True)
+    return scan_data.pixels, new_meta_data
+
+def main():
+    sharedLocation = "/Users/nathaniel_wk/Documents/NUANCE Files/NuScope/Sample File Formats/"
+    
+    #Hitachi Test File
+    hitachiS4800_filename = sharedLocation+"S4800/" + "tin.tif"
+    hitachiS4800_metafilename = sharedLocation+"S4800/"+"tin.TXT"
+    
+    #Quanta Test File 
+    quanta_filename = sharedLocation + "Quanta/"+"ResolutionTest_SE_10kx_5kv_008.tif"
+    quanta_metafilename = sharedLocation + "Quanta/"+"ResolutionTest_SE_10kx_5kv_008-COPIED.TXT"
+    
+    #JEOL 7900 Test File 
+    location = "/Users/nathaniel_wk/Documents/NUANCE Files/NuScope/Sample File Formats/7900/"
+    jeol7900_filename = sharedLocation +"7900/"+"tt_LED_2.tif"
+    jeol7900_metafilename = sharedLocation + "7900/"+ "tt_LED_2.TXT"   
+    
+    S4800_idata,S4800_mdata = HitachiSEMImageLoad(hitachiS4800_filename,hitachiS4800_metafilename)
+    quanta_idata,quanta_mdata = QuantaSEMImageLoad(quanta_filename,quanta_metafilename)
+    jeol7900_idata,jeol7900_mdata = JEOL7900SEMImageLoad(jeol7900_filename, jeol7900_metafilename)
+    plt.imshow(S4800_idata)
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ NUScopeGUI_v0.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 15:31:55 2020
+@author: Nathaniel Kabat
+
+"""
+
+import sys
+import os 
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QHBoxLayout,QVBoxLayout, QFileDialog,QWidget, QGridLayout,  QTextBrowser,QTableWidget,QTableWidgetItem
+import matplotlib
+import numpy as np
+import h5py
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+from SEMImageLoadLibraries_v0 import *
+matplotlib.use('Qt5Agg')
+
+progname = os.path.basename(sys.argv[0])
+progversion = "0.1"   
+
+class figureWindow(QWidget):  
+    def __init__(self,parent=None):
+        super(figureWindow, self).__init__(parent)
+        self.setLayout(QVBoxLayout())
+        self.figure = Figure(figsize=(12,12))
+        self.canvas = FigureCanvas(self.figure)
+        self.button = QPushButton('Load File and Plot')
+        #self.ax = self.figure.add_subplot(111)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.layout().addWidget(self.toolbar)
+        self.layout().addWidget(self.canvas)
+        self.layout().addWidget(self.button)
+        #self.canvas.setSizePolicy(QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding)
+        #self.canvas.updateGeometry()
+        
+    
+class logWindow(QWidget):
+    def __init__(self,parent=None):
+        super(logWindow, self).__init__(parent)
+        self.setLayout(QVBoxLayout())
+        self.textw = QTextBrowser()
+        self.saveLogButton = QPushButton('Save Log File')
+        self.saveLogButton.clicked.connect(self.saveLogFile) 
+        self.layout().addWidget(QtWidgets.QLabel("Log File"))
+        self.layout().addWidget(self.textw)
+        self.layout().addWidget(self.saveLogButton)
+    def saveLogFile(self):
+        np.savetxt('LogFileOutPut.txt',[self.textw.toPlainText()], fmt='%s')
+    
+
+        
+class Window(QDialog):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+        self.setWindowTitle("NUScope Image Analysis (GUI v0)")
+        self.setGeometry(800,200,1200,600)
+        self.setLayout(QHBoxLayout())
+      
+        #Create widget for figure plotting
+        figwig = QWidget()
+        figwig.resize(600, 600)
+        figwig.setLayout(QVBoxLayout())
+        figwig.figure = Figure()
+        figwig.canvas = FigureCanvas(figwig.figure)
+        figwig.setSizePolicy(QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding)
+        figwig.setMinimumSize(200,200)
+        figwig.canvas.updateGeometry()           
+        figwig.button = QPushButton('Load File and Plot')
+
+        #self.ax = self.figure.add_subplot(111)
+        figwig.toolbar = NavigationToolbar(figwig.canvas, figwig)
+        figwig.layout().addWidget(figwig.toolbar)
+        figwig.layout().addWidget(figwig.canvas)
+        figwig.layout().addWidget(figwig.button)
+
+        #Create widget for metadata:
+        metawidget =QWidget()
+        metawidget.resize(150,600)
+        metawidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding)
+        metawidget.setMinimumSize(350,50)               
+        metawidget.setLayout(QVBoxLayout())
+        metawidget.table = QTableWidget()
+        metawidget.table.setColumnCount(1)
+        metawidget.table.setRowCount(0)
+        metawidget.table.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)        
+        metawidget.layout().addWidget(QtWidgets.QLabel("Metadata Table"))
+        metawidget.layout().addWidget(metawidget.table)         
+        
+        #Create widget for log file show
+        logwidget = QWidget()
+        logwidget.resize(250,600)
+        logwidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding)
+        logwidget.setMinimumSize(150,50)
+        logwidget.setLayout(QVBoxLayout())
+        logwidget.textw = QTextBrowser()
+        logwidget.saveLogButton = QPushButton('Save Log File')
+        logwidget.layout().addWidget(QtWidgets.QLabel("Log File"))
+        logwidget.layout().addWidget(logwidget.textw)
+        logwidget.layout().addWidget(logwidget.saveLogButton)
+
+
+        #Button commands that reference both the figure widget and the log widget
+        figwig.button.clicked.connect(self.clickedFilewMetaDataLoad(figwig,logwidget,metawidget)) 
+        logwidget.saveLogButton.clicked.connect(self.saveLogFile(logwidget))         
+
+
+        # set the layout
+        self.layout().addWidget(figwig,0)
+        self.layout().addWidget(metawidget,1)
+        self.layout().addWidget(logwidget,2)
+
+    def saveLogFile(self,logwidget):
+        def runsave():
+            np.savetxt('Output.txt',[logwidget.textw.toPlainText()], fmt='%s')
+        return runsave
+    def clickedFilewMetaDataLoad(self,specificFig,logwidget,metawidget):
+        def openFile():
+            self.dlg = QFileDialog()
+            #filters = "Hitachi SEM Image (*.tif *.tiff);;JEOL SEM Image (*.tif *.tiff);;FEI Quanta SEM Image (*.tif *.tiff)"
+            filters = "Generic Image File (*.tiff *.tif *.png *.jpg *.jpeg);; Hitachi SEM Image (*.tif *.tiff);;JEOL SEM Image (*.tif *.tiff);;FEI Quanta SEM Image (*.tif *.tiff);; Bruker AFM (*.spm)"
+            self.fname, self.filterChoice = self.dlg.getOpenFileName(self, "Select an Image file...",filter=filters,options=QFileDialog.DontUseNativeDialog)            
+             
+            #select correct image loading function based on choice from file menu
+            if "Hitachi" in self.filterChoice:
+                self.metafname, _ = self.dlg.getOpenFileName(self, "Select an Image file...",filter="Text File (*.txt)",options=QFileDialog.DontUseNativeDialog)                            
+                self.imdata,self.metadata = HitachiSEMImageLoad(self.fname,self.metafname)          
+            elif "Quanta" in self.filterChoice:
+                self.metafname, _ = self.dlg.getOpenFileName(self, "Select an Image file...",filter="Text File (*.txt)",options=QFileDialog.DontUseNativeDialog)                            
+                self.imdata,self.metadata = QuantaSEMImageLoad(self.fname,self.metafname)                
+            elif "JEOL" in self.filterChoice:
+                self.metafname, _ = self.dlg.getOpenFileName(self, "Select an Image file...",filter="Text File (*.txt)",options=QFileDialog.DontUseNativeDialog)                            
+                self.imdata,self.metadata = JEOL7900SEMImageLoad(self.fname,self.metafname)
+            elif "Generic Image" in self.filterChoice:
+                self.imdata,self.metadata = GenericImageOpenTIFF(self.fname)
+            elif "Bruker" in self.filterChoice:
+                self.imdata,self.metadata = BrukerAFMImageLoad(self.fname)
+            
+            #convert metadata to table
+            vertHeaders = []
+            print(self.metadata.keys())
+            for n,key in enumerate(self.metadata.keys()):
+                vertHeaders.append(key)
+                newitem = QTableWidgetItem(self.metadata[key])
+                metawidget.table.setItem(0, n, newitem)
+                metawidget.table.setVerticalHeaderLabels(vertHeaders)
+                metawidget.table.resizeColumnsToContents()
+                metawidget.table.resizeRowsToContents()
+                metawidget.table.setRowCount(len(self.metadata))
+            
+            logwidget.textw.append(f"Opened file: {self.fname} \n")                      
+            logwidget.textw.append(f"File Type Selected: {self.filterChoice} \n")
+            
+
+            specificFig.figure.clear() #clear figure
+            # create an axis
+            ax = specificFig.figure.add_subplot(111)
+
+
+            ax.imshow(self.imdata)  # plot data
+            ax.axis("off")
+            specificFig.figure.tight_layout()
+            specificFig.canvas.draw() # refresh canvas
+
+        return openFile
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    mainWindow = Window()
+    mainWindow.show()
+    sys.exit(app.exec_())        
+        
+        
+        
+if __name__ == "__main__":
+    main()
+
